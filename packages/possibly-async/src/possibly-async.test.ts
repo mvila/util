b28a42cb9d8eafc7dfec36e3e377fd60294089b4
Manuel Vila <mvila@3base.com>
2020-05-09 13:08:44 +0900

    possibly-async: Implement forEach()

--- packages/possibly-async/src/possibly-async.test.ts
+++ packages/possibly-async/src/possibly-async.test.ts
@@ -36,21 +36,73 @@ describe('possibly-async', () => {
     ta.assert<ta.Equal<typeof r7, PromiseLike<PromiseLike<number>>>>();
     await expect(r7).resolves.toBe(3);
 
-    const r8 = possiblyAsync(makePromise('ERROR'), (value) => value + 1);
-    ta.assert<ta.Equal<typeof r8, PromiseLike<number>>>();
-    await expect(r8).rejects.toBe('ERROR');
+    (function (valueProvider: () => number | PromiseLike<number>) {
+      const r8 = possiblyAsync(valueProvider(), (value) => String(value + 1));
+      ta.assert<ta.Equal<typeof r8, string | PromiseLike<string>>>();
+    })(() => 1);
 
-    const r9 = possiblyAsync(1, (_value) =>
-      possiblyAsync(makePromise('ERROR'), (value) => value + 1)
-    );
+    const r9 = possiblyAsync(makePromise('ERROR'), (value) => value + 1);
     ta.assert<ta.Equal<typeof r9, PromiseLike<number>>>();
     await expect(r9).rejects.toBe('ERROR');
+
+    const r10 = possiblyAsync(1, (_value) =>
+      possiblyAsync(makePromise('ERROR'), (value) => value + 1)
+    );
+    ta.assert<ta.Equal<typeof r10, PromiseLike<number>>>();
+    await expect(r10).rejects.toBe('ERROR');
+  });
+
+  test('possiblyAsync.forEach()', async () => {
+    const a1: number[] = [];
+    const r1 = possiblyAsync.forEach(
+      [1, 2],
+      (v) => a1.push(v + 1),
+      () => a1
+    );
+    ta.assert<ta.Equal<typeof r1, number[]>>();
+    expect(r1).toEqual([2, 3]);
+
+    const a2: number[] = [];
+    const r2 = possiblyAsync.forEach(
+      [1, 2],
+      (v) => possiblyAsync(v + 1, (newV) => a2.push(newV)),
+      () => a2
+    );
+    ta.assert<ta.Equal<typeof r2, number[]>>();
+    expect(r2).toEqual([2, 3]);
+
+    const a3: number[] = [];
+    const r3 = possiblyAsync.forEach(
+      [1, 2],
+      (v) => makePromise(a3.push(v + 1)),
+      () => a3
+    );
+    ta.assert<ta.Equal<typeof r3, PromiseLike<number[]>>>();
+    await expect(r3).resolves.toEqual([2, 3]);
+
+    const a4: number[] = [];
+    const r4 = possiblyAsync.forEach(
+      [1, 2],
+      (v) => possiblyAsync(makePromise(v + 1), (newV) => a4.push(newV)),
+      () => a4
+    );
+    ta.assert<ta.Equal<typeof r4, PromiseLike<number[]>>>();
+    await expect(r4).resolves.toEqual([2, 3]);
+
+    const a5: number[] = [];
+    const r5 = possiblyAsync.forEach(
+      [1, 2],
+      (v) => (v === 1 ? makePromise(a5.push(v + 1)) : a5.push(v + 1)),
+      () => a5
+    );
+    ta.assert<ta.Equal<typeof r5, number[] | PromiseLike<number[]>>>();
+    await expect(r5).resolves.toEqual([2, 3]);
   });
 });
 
-function makePromise<V>(value: 'ERROR'): PromiseLike<never>;
-function makePromise<V>(value: V): PromiseLike<V>;
-function makePromise<V>(value: V): PromiseLike<V> {
+function makePromise<V>(value?: 'ERROR'): PromiseLike<never>;
+function makePromise<V>(value?: V): PromiseLike<V>;
+function makePromise<V>(value?: V): PromiseLike<V> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (typeof value === 'string' && value === 'ERROR') {
--- packages/possibly-async/src/possibly-async.ts
+++ packages/possibly-async/src/possibly-async.ts
@@ -1,10 +1,42 @@
 import isPromise from 'is-promise';
 
-export function possiblyAsync<V, R>(
-  promise: PromiseLike<V>,
-  callback: (value: V) => R
-): PromiseLike<R>;
-export function possiblyAsync<V, R>(value: V, callback: (value: V) => R): R;
-export function possiblyAsync<V, R>(valueOrPromise: V | PromiseLike<V>, callback: (value: V) => R) {
+export type PromiseLikeValue<Input> = Input extends PromiseLike<infer Value> ? Value : Input;
+
+export function possiblyAsync<
+  ValueOrPromise,
+  CallbackResult,
+  Value = PromiseLikeValue<ValueOrPromise>,
+  Result = ValueOrPromise extends PromiseLike<Value> ? PromiseLike<CallbackResult> : CallbackResult
+>(valueOrPromise: ValueOrPromise, callback: (value: Value) => CallbackResult): Result;
+export function possiblyAsync(valueOrPromise: any, callback: (value: any) => any) {
   return isPromise(valueOrPromise) ? valueOrPromise.then(callback) : callback(valueOrPromise);
+}
+
+export namespace possiblyAsync {
+  export function forEach<
+    Value,
+    IterateeResultValueOrPromise,
+    CallbackResult,
+    IterateeResultValue = PromiseLikeValue<IterateeResultValueOrPromise>,
+    Result = IterateeResultValueOrPromise extends PromiseLike<IterateeResultValue>
+      ? PromiseLike<CallbackResult>
+      : CallbackResult
+  >(
+    iterable: Iterable<Value>,
+    iteratee: (value: Value, index: number) => IterateeResultValueOrPromise,
+    callback: () => CallbackResult
+  ): Result {
+    const iterator = iterable[Symbol.iterator]();
+    let index = 0;
+
+    const iterate = function (): void | PromiseLike<void> {
+      const {value, done} = iterator.next();
+
+      if (!done) {
+        return possiblyAsync(iteratee(value, index++), iterate);
+      }
+    };
+
+    return possiblyAsync(iterate(), callback);
+  }
 }
