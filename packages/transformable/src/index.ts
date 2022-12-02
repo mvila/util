1ef7ce2a2020b59590406eecd0918ef760eb9eea
Manuel Vila <mvila@3base.com>
2022-12-03 08:46:06 +0900

    transformable: Extract to a dedicated repository
    New repository: https://github.com/mvila/transformable

--- packages/transformable/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './transformable';
-export * from './decorators';
-export * from './functions';
-export * from './types';
--- packages/transformable/src/types.ts
+++
@@ -1,7 +0,0 @@
-export type AttributeTransformationMap = Map<string, Transformation>;
-
-export type Transformation = {input?: Transformer; output?: Transformer};
-
-export type Transformer = (value: any, context: TransformerContext) => any;
-
-export type TransformerContext = {source?: string; target?: string};
